5ac5195d034065829175bf93f2d9942d86c7d26b
Ilya <godfrid7@mail.ru>
2022-12-21 22:25:36 +0300

    Add

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    String name;
+    String passport;
+    String patronymic;
+    String phone;
+    String surname;
+    boolean subscription;
+    FormDate birthday;
+    int day;
+    int month;
+    int year;
+}
+
+
